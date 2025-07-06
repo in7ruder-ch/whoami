29b4715c2d7dd6f928b934f2fbd26cf142b2e5c9
Matias Vanarelli <in7ruder@proton.me>
2025-07-06 20:24:41 +0200

    form

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 function toggleMenu() {
   document.getElementById('navbar').classList.toggle('active');
 }
+
+const form = document.querySelector("form");
+const thankYou = document.getElementById("thank-you");
+
+form.addEventListener("submit", async function (e) {
+  e.preventDefault();
+  const formData = new FormData(form);
+  if (formData.get("_gotcha")) return; // bot trap
+
+  const response = await fetch(form.action, {
+    method: "POST",
+    body: formData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    form.style.display = "none";
+    thankYou.style.display = "block";
+  } else {
+    alert("There was a problem submitting your form. Please try again.");
+  }
+});
